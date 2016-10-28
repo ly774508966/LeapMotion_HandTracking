e9fc58671a56ee4cfe1e7beba42577606a5b9d57
Aravind Sivaramakrishnan <aravind.siva@rutgers.edu>
2016-10-28 01:25:11 -0400

    Play works?

--- solution/Play.hpp
+++ solution/Play.hpp
@@ -1,27 +1,14 @@
 #include "Leap.h"
 #include "LeapRecorder.hpp"
 #include <iostream>
-#include <Windows.h>
+// #include <Windows.h>
+
+using namespace Leap;
 
 class Play {
 public:
-	Play::Play() {};
-	Play::~Play() {};
-	int i = 0;
-	int Play::play(std::string filename)
-	{
-	Leap::Controller controller = Leap::Controller();
-	LeapRecorder recorder;
-
-		if (!recorder.Load(filename)) {
-			return 2;
-		}
-		recorder.Play();
-		while (recorder.get_state() != LeapRecorder::STATE_IDLE) {
-			Leap::Frame frame = recorder.GetCurrentFrame();
-			std::cout << "Got frame id #" << frame.id() << std::endl;
-			//Sleep(13000);
-		}
-		return 0;
-	}
+	Play() {};
+	~Play() {};
+	void displayFrame(Frame frame);
+	int play(std::string filename);
 };
--- solution/Record.hpp
+++ solution/Record.hpp
@@ -3,42 +3,13 @@
 
 class Record {
 public:
-	int i = 0;
-	Record::Record() {};
-	Record::~Record() {};
-	int Record::record(std::string filename)
-	{
-
-		FILE* test = fopen(filename.c_str(), "wb");
-		if (!test) {
-			std::cerr << "can not open " << filename << " for writing..." << std::endl;
-			return 2;
-		}
-		fclose(test);
-		Leap::Controller controller;
-		LeapRecorder recorder;
-		controller.addListener(recorder);
-		controller.setPolicy(Leap::Controller::POLICY_BACKGROUND_FRAMES);
-
-		if (!controller.isConnected()) {
-			std::cerr << "error no LeapMotion found!" << std::endl;
-			return 3;
-		}
-
-		// Record until Enter is pressed
-		recorder.Record();
-		std::cout << "Press Enter to quit..." << std::endl;
-		std::cin.get();
-		recorder.Stop();
-
-		if (!recorder.Save(filename)) {
-			std::cerr << "error during saving." << std::endl;
-			return 2;
-		}
-
-		controller.removeListener(recorder);
-		return 0;
-	}
+	// int i = 0;
+	// Record::Record() {};
+	// Record::~Record() {};
+	Record() {};
+	~Record() {};
+	// int Record::record(std::string filename)
+	int record(std::string filename);
 
 };
 
